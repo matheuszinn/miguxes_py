ab657b644e90f3a04ef8f940faa81fd84861246f
matheuszinn <theustloz@gmail.com>
2022-01-26 16:12:19 -0300

    :sparkles: Aparentemente primeira implementação funcional

---
+++ main.py
@@ -0,0 +1,11 @@
+from miguxes_py.miguxes import miguxar
+from miguxes_py.dialetos import DialetoMiguxes
+
+if __name__ == "__main__":
+    print(miguxar("""
+    Você acha engraçado tirar screenshots dos NFTs das pessoas, hein?
+    Roubo de propriedade é uma piada para você? Eu quero que você saiba que o blockchain não mente.
+    Eu possuo-o.
+    Mesmo se você salvá-lo, é minha propriedade.
+    Você está com raiva por não possuir a arte que possuo.
+    Exclua essa captura de tela!.""", DialetoMiguxes.NEO_MIGUXES))
